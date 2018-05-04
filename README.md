3cbfdd5bcdf3a8d2d663cb1bf03ee0bddb12c3b3
ccorbi <carles.corbi@gmail.com>
2018-05-03 20:16:39 -0400

    anaconda package

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# NGSKit
 
 [![travis](https://img.shields.io/travis/kimlaborg/kmtools.svg?style=flat-square)](https://travis-ci.org/kimlaborg/NGSKit/)
 [![codecov](https://img.shields.io/codecov/c/github/kimlaborg/kmtools.svg?style=flat-square)](https://codecov.io/gh/kimlaborg/NGSKit)
+
+# NGSKit
 
 Collection basic tools to preprocess NGS data for the lab Pipelines.
 
--- setup.py
+++ setup.py
@@ -1,16 +1,31 @@
 from setuptools import setup, find_packages
 
+
+requirements = [
+            'pandas',
+            'numpy',
+            'scikit-learn',
+            'pandas']
+
+
 setup(
     name='ngskit',
-    version='0.0.1',
+    version='0.1',
+    packages=  find_packages(),
     author='kimlab.org',
     author_email='carles.corbi@kimlab.org',
     url = "http://kimlab.org",
     description = ("Small kit of Tools for preprocess NGS data. Customize for Kimlab's data pipelines"),
     packages=['ngskit','ngskit.utils','tests'],
-    install_requires=["pandas",
-                      "python-Levenshtein",
-    ]
+    install_requires=requirements,
+    
+    classifiers=[
+       "License :: OSI Approved :: MIT License",
+       "Programming Language :: Python :: 3",
+       "Topic :: Scientific/Engineering :: Bio-Informatics",
+       ],
+    license='MIT',
+
 
 
 
